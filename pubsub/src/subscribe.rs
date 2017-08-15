8ecfcb8ae68c9ca246f6824e3a623cd5db5b1cd5
Kimi <kimi.kan@xxxx.com>
2017-08-15 15:17:09 +0800

    shrink

--- pubsub/src/subscribe.rs
+++ pubsub/src/subscribe.rs
@@ -5,29 +5,15 @@ use std::io::Read;
 
 pub fn loop_sub(topic:&str, url:&str)->io::Result<()> {
     let mut socket = Socket::new(Protocol::Sub)?;
-    let setopt = socket.subscribe(topic.as_bytes());
+    socket.subscribe(topic.as_bytes())?;
     let mut endpoint = socket.connect(url)?;
-
-    match setopt {
-        Ok(_) => println!("Subscribed to '{:?}'.", topic),
-        Err(err) => println!("Client failed to subscribe '{}'.", err)
-    }
 
     let mut msg = String::new();
     loop {
-        match socket.read_to_string(&mut msg) {
-            Ok(_) => {
-                println!("Recv '{}'.", msg);
-                msg.clear()
-            },
-            Err(err) => {
-                println!("Client failed to receive msg '{}'.", err);
-                break
-            }
-        }
+        socket.read_to_string(&mut msg)?;
+        println!("Recv '{}'.", msg);
+        msg.clear();
     }
-
-    endpoint.shutdown().unwrap();
-
+    endpoint.shutdown()?;
     Ok(())
 }
