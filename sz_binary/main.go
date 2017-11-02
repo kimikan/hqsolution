656dead359d96b54ec0af1b469a973f2755a1e3b
Kimi Kan <k32459871@126.com>
2017-11-02 17:18:18 +0800

    Update main.go

--- sz_binary/main.go
+++ sz_binary/main.go
@@ -8,6 +8,8 @@ import (
 	"io"
 	"net"
 	"os"
+	"sync"
+	"time"
 )
 
 //MsgHead defines a
@@ -16,6 +18,52 @@ type MsgHead struct {
 	_msgType    uint32
 	_bodyLength uint32
 }
+
+//Stock defines a struct
+type Stock struct {
+	sync.RWMutex
+	code         string
+	marginStatus uint32
+}
+
+func (p *Stock) setMargin(v uint32) {
+	p.Lock()
+	p.marginStatus = v
+	p.Unlock()
+}
+
+//Stocks defines a thread-safe stock list
+type Stocks struct {
+	sync.RWMutex
+	stocks map[string]*Stock
+}
+
+func newStocks() *Stocks {
+	stocks := &Stocks{
+		stocks: make(map[string]*Stock),
+	}
+
+	return stocks
+}
+
+func (p *Stocks) getOrDefaultStock(code string) *Stock {
+	p.RLock()
+	stock, ok := p.stocks[code]
+	p.RUnlock()
+
+	if !ok {
+		stock = &Stock{
+			code: code,
+		}
+		p.Lock()
+		p.stocks[code] = stock
+		p.Unlock()
+	}
+
+	return stock
+}
+
+var stocks = newStocks()
 
 func struct2Bytes(msg interface{}) ([]byte, error) {
 	buf := bytes.NewBuffer(nil)
@@ -114,6 +162,7 @@ func getMessage(conn net.Conn) (uint32, uint32, []byte, error) {
 		return 0, 0, nil, err
 	}
 
+	var buf []byte
 	if bodyLen > 0 {
 		buf := make([]byte, bodyLen)
 
@@ -122,20 +171,19 @@ func getMessage(conn net.Conn) (uint32, uint32, []byte, error) {
 			return 0, 0, nil, err2
 		}
 
-		var checksum uint32
-		err2 = binary.Read(conn, binary.BigEndian, &checksum)
-		if err2 != nil {
-			return 0, 0, nil, err2
-		}
-
 		if n != int(bodyLen) {
-			return msgType, checksum, nil, errors.New("Wrong size expect")
+			fmt.Println(n, bodyLen)
+			return msgType, 0, nil, errors.New("Wrong size expect")
 		}
-
-		return msgType, checksum, buf, nil
 	}
 
-	return msgType, 0, nil, errors.New("Wrong msg")
+	var checksum uint32
+	err2 := binary.Read(conn, binary.BigEndian, &checksum)
+	if err2 != nil {
+		return 0, 0, nil, err2
+	}
+
+	return msgType, checksum, buf, nil
 }
 
 func handleChannelHeartBeat(conn net.Conn, messageBody []byte) {
@@ -146,9 +194,42 @@ func handleResentMsg(conn net.Conn, messageBody []byte) {
 
 }
 
-func handleUserReport(conn net.Conn, messageBody[]byte)
-{
-    
+func handleUserReport(conn net.Conn, messageBody []byte) {
+	type reportMsg struct {
+		origTime    int64
+		versionCode [16]byte
+		userNum     uint16
+	}
+
+	if len(messageBody) >= 26 {
+		msg := &reportMsg{}
+		msg.origTime = int64(binary.BigEndian.Uint64(messageBody))
+		copy(msg.versionCode[:], messageBody[8:])
+		msg.userNum = binary.BigEndian.Uint16(messageBody[24:])
+		fmt.Println("ReportMessage: ", msg)
+	} else {
+		fmt.Println("Wrong message", len(messageBody))
+	}
+}
+
+func handleChannelStatistic(conn net.Conn, messageBody []byte) {
+	//do nothing about snapshot statistic message
+}
+
+func handleRealtimeStatus(conn net.Conn, messageBody []byte) {
+	type Switch struct {
+		switchType   uint16
+		switchStatus uint16
+	}
+
+	type realState struct {
+		origTime         int64
+		channelNo        uint16
+		securityID       [8]byte
+		securityIDSource [4]byte //102 shenzhen, 103 hongkong
+		financialStatus  [8]byte
+		switchers        []Switch
+	}
 }
 
 func handleMessage(conn net.Conn, msgType uint32, messageBody []byte) {
@@ -160,10 +241,46 @@ func handleMessage(conn net.Conn, msgType uint32, messageBody []byte) {
 		handleResentMsg(conn, messageBody)
 	case 390093:
 		handleUserReport(conn, messageBody)
+	case 390090:
+		handleChannelStatistic(conn, messageBody)
+	case 390013:
+		handleRealtimeStatus(conn, messageBody)
 	default:
 		fmt.Println(msgType, messageBody)
 	}
 
+}
+
+func heartbeat(conn net.Conn) error {
+	type heartbeatMsg struct {
+		header   MsgHead
+		checksum uint32
+	}
+
+	msg := heartbeatMsg{
+		header: MsgHead{
+			_msgType:    3,
+			_bodyLength: 0,
+		},
+		checksum: 0,
+	}
+
+	bs, err := struct2Bytes(&msg)
+	if err != nil {
+		return err
+	}
+	checksum := generateCheckSum(bs[:len(bs)-4])
+
+	binary.BigEndian.PutUint32(bs[len(bs)-4:], checksum)
+	n, err2 := conn.Write(bs)
+	if err2 != nil {
+		return err2
+	}
+
+	if n != len(bs) {
+		return errors.New("write failed")
+	}
+	return nil
 }
 
 func main() {
@@ -181,6 +298,18 @@ func main() {
 		return
 	}
 
+	go func() {
+		for {
+			select {
+			case <-time.After(time.Duration(5) * time.Second):
+				if heartbeat(conn) != nil {
+					fmt.Println("heartbeat failed")
+					break
+				}
+			}
+		}
+	}()
+
 	for {
 		msgType, _, messageBody, err2 := getMessage(conn)
 		if err2 != nil {
@@ -188,7 +317,14 @@ func main() {
 			break
 		}
 
-		handleMessage(conn, msgType, messageBody)
+		if messageBody == nil {
+			fmt.Println("heartbeat message")
+			if heartbeat(conn) != nil {
+				fmt.Println("heartbeat failed")
+			}
+		} else {
+			handleMessage(conn, msgType, messageBody)
+		}
 	}
 }
 
