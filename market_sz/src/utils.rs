0d3960837b500b0a2922fe0da475ce1afa4f9765
Kimi Kan <k32459871@126.com>
2017-11-14 13:01:48 +0800

    Update utils.rs

--- market_sz/src/utils.rs
+++ market_sz/src/utils.rs
@@ -4,6 +4,42 @@
 use log;
 use log::{LogRecord, LogLevel, LogMetadata, LogLevelFilter};
 
+use serde_json;
+use serde::{ Deserialize};
+use std::io;
+use std::fs::OpenOptions;
+
+#[derive(Deserialize, Debug)]
+pub struct Configuration {
+    pub _addr : String,
+}
+
+impl Configuration {
+
+    pub fn load()->io::Result<Configuration> {
+        let file = OpenOptions::new()
+                    .read(true).open("default.cfg")?;
+        
+        use std::io::BufReader;
+        let mut buf_reader = BufReader::new(file);
+        let mut contents = String::new();
+
+        use std::io::Read;
+        buf_reader.read_to_string(&mut contents)?;
+
+        let r : serde_json::Result<Configuration> = serde_json::from_str(&contents);
+   
+        if let Ok(c) = r {
+            return Ok(c);
+        } else {
+            println!("{:?}", r);
+        }
+
+        Err(io::Error::from(io::ErrorKind::InvalidData))
+    }
+}
+
+
 pub struct SimpleLog ;
 
 impl log::Log for SimpleLog {
@@ -13,7 +49,7 @@ impl log::Log for SimpleLog {
 
     fn log(&self, r: &LogRecord) {
         if self.enabled(r.metadata()) {
-            use std::fs::OpenOptions;
+            
             let path = "default.log";
             let file_op = OpenOptions::new()
                 .create(true).write(true).append(true)
@@ -21,7 +57,8 @@ impl log::Log for SimpleLog {
             
             if let Ok(mut f) = file_op {
                 use std::io::Write;
-                println!("{:?}", r.args());
+                
+				println!("{:?}", r.args());
                 if let Err(e) = writeln!(f, "{:?}", r.args()) {
                     println!("{:?}", e);
                 }
